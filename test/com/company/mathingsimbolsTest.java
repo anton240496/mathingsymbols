a5dde476637444d2324b1224b76b201ad2820db3
anton <anton240496@mail.ru>
2020-05-09 22:13:00 +0300

    исправленно

--- test/com/company/mathingsimbolsTest.java
+++ test/com/company/mathingsimbolsTest.java
@@ -10,7 +10,8 @@ class mathingsimbolsTest {
     void test() {
         mathingsimbols a = new mathingsimbols("accvvdv");
         System.out.println(a.getText());
-        assertNotEquals("accvvdv",a.getText());
+        assertEquals("adv",a.getText());
+
 
     }
 
@@ -18,28 +19,28 @@ class mathingsimbolsTest {
     void test1() {
         mathingsimbols a = new mathingsimbols("aa89bbffc");
         System.out.println(a.getText());
-        assertNotEquals("aa89bbffc",a.getText());
+        assertEquals("89c",a.getText());
     }
 
     @Test
     void test2() {
         mathingsimbols a = new mathingsimbols("aabbcc");
         System.out.println(a.getText());
-        assertNotEquals("aabbcc",a.getText());
+        assertEquals("",a.getText());
     }
 
     @Test
     void test3() {
         mathingsimbols a = new mathingsimbols("AnnA?!");
         System.out.println(a.getText());
-        assertNotEquals("AnnA?!",a.getText());
+        assertEquals("AA?!",a.getText());
     }
 
     @Test
     void test4() {
         mathingsimbols a = new mathingsimbols("AnnA?!12234aa34");
         System.out.println(a.getText());
-        assertNotEquals("AnnA?!12234aa34",a.getText());
+        assertEquals("AA?!1223434",a.getText());
 
     }
 }
